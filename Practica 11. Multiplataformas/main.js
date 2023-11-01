14d8b44adc639ee6f6bb31c4f9f5d92248b4f91a
Vanessa <92649962+Vanne28@users.noreply.github.com>
2023-10-31 21:05:55 -0600

    Add files via upload

---
+++ Practica 11. Multiplataformas/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('app/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () =>{
+if (process.platform !== 'darwin') {
+    app.quit();
+}
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
